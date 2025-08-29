ffe9e091b2c6fb9cb83ca6743e7757fa9ed60464
Rin Le <rinle.it@gmail.com>
2025-08-29 17:08:53 +0700

    refactor: optimize Dockerfiles and update start scripts for uv compatibility

--- acceptor/start.sh
+++ acceptor/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -fr ./Sessions && mkdir -p ./Sessions && chmod 755 ./Sessions
 sed -i "s/^SocketAcceptPort=.*/SocketAcceptPort=${APP_PORT}/" server.cfg
-python server.py server.cfg
+uv run python server.py server.cfg
